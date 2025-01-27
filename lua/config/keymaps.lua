5cd814dcc5d1cd725a95eb1f459656bcdc5c0d49
g-imhoff <guillaume.imhoff170804@gmail.com>
2025-01-27 11:48:39 +0000

    modif

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Insert mode keybindings for < and > using Ctrl+Alt+j and Ctrl+Alt+k
 vim.keymap.set('i', '<C-A-j>', '<', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-A-k>', '>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
